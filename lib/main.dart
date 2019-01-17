a0ce93f444616dd55c8ae77ead506fa274fa95ef
Ersin <ersin.okur@gmail.com>
2019-01-17 15:28:58 +0300

    tab double tap long tap fetch

--- lib/main.dart
+++ lib/main.dart
@@ -21,13 +21,65 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int tabsCount = 0;
+  int doubleTabsCount = 0;
+  int longPressCount = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Gestures Demo"),
       ),
-      body: Center(),
+      body: GestureDetector(
+        onTap: () {
+          setState(() {
+            tabsCount++;
+          });
+        },
+        onDoubleTap: () {
+          setState(() {
+            doubleTabsCount++;
+          });
+        },
+        onLongPress: () {
+          setState(() {
+            longPressCount++;
+          });
+        },
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              left: 50.0,
+              top: 50.0,
+              child: Container(
+                width: 30.0,
+                height: 30.0,
+                decoration: BoxDecoration(
+                  color: Colors.lightBlue,
+                ),
+              ),
+            ),
+            Positioned(
+              left: 90.0,
+              top: 50.0,
+              child: Container(
+                width: 30.0,
+                height: 30.0,
+                decoration: BoxDecoration(
+                  color: Colors.lightGreen,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: Material(
+          child: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Text(
+            "Tab: $tabsCount, double tap: $doubleTabsCount, long press: $longPressCount"),
+      )),
     );
   }
 }
